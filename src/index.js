214fd78732405e045bc21e7dbfa5cb72f82cc31c
Crab <crabcodes28@gmail.com>
2023-06-23 18:58:22 +0200

    Add dummy content structure

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './reset.css';
 import './style.css';
 import './components/header/header.css';
 import './components/side/side.css';
+import './components/content/content.css';
 
 import { createHeader } from './components/header/header';
 import { createAside } from './components/side/side';
