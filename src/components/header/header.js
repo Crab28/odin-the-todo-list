5eea2d348c2ac4b49325ed5f8fc876256fedaacc
Crab <crabcodes28@gmail.com>
2023-06-25 20:32:14 +0200

    Add menu button functionality

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ function createHeader() {
     const header = document.getElementById('header');
     header.innerHTML = `
     <div class="header-content">
-        <button type="button" class="btn">
+        <button type="button" class="btn" id="menu-btn">
             <div class="btn-icon"></div>
         </button>
         <h1>The Big To-Do</h1>
@@ -11,6 +11,16 @@ function createHeader() {
             <span class="slider round"></span>
         </label>
     </div>`
+
+    addMenuListener();
+}
+
+function addMenuListener() {
+    const aside = document.getElementById('aside');
+    const menuEl = document.getElementById('menu-btn');
+    menuEl.addEventListener('click', () => {
+        aside.classList.toggle('remove-content');
+    });
 }
 
 export { createHeader }
